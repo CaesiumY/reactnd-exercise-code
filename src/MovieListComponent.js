e3954535de6f84579d7490ff5f1df53a41d1fde8
caesiumy <mn0658@naver.com>
2020-05-01 23:24:30 +0900

    feat: Add MovieListComponent

--- src/MovieListComponent.js
+++ src/MovieListComponent.js
@@ -1,11 +1,19 @@
-import React, {component} from "react"
+import React, { Component } from "react";
 
-export class MovieListComponent extends component {
-	render(){
-      return (
-        <ol>
-        	<li>hello world</li>
-        </ol>
-      )
-    }
+export default class MovieListComponent extends Component {
+  render() {
+    const { profiles, movies, users } = this.props;
+    return (
+      <ul>
+        {profiles.map((profile) => (
+          <li key={profile.id}>
+            <p>
+              {users[profile.userID].name}'s favorite movie is{" "}
+              {movies[profile.favoriteMovieID].name}
+            </p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
