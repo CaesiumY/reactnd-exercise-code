9321e1965b9ead1239a0265b2b0a58b584f2747a
CaesiumY <dbs2636@gmail.com>
2020-05-01 14:00:04 +0000

    feat: Add exercise 1 - passing data

---
+++ src/MovieListComponent.js
@@ -0,0 +1,11 @@
+import React, {component} from "react"
+
+export class MovieListComponent extends component {
+	render(){
+      return (
+        <ol>
+        	<li>hello world</li>
+        </ol>
+      )
+    }
+}
