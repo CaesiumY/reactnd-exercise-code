e24adf864cf5763a1a430d85e9518919cd75f62b
caesiumy <mn0658@naver.com>
2020-05-03 00:05:47 +0900

    feat: Add controlled component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    echo: "This should mirror the text you typed into the input field!",
+  };
+
   render() {
+    const handleMsg = (msg) =>
+      this.setState((state) => ({
+        echo: msg,
+      }));
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,9 +20,16 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input
+            type="text"
+            placeholder="Say Something"
+            onChange={(event) => {
+              handleMsg(event.target.value);
+            }}
+            value={this.state.echo}
+          />
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.echo}</p>
         </div>
       </div>
     );
